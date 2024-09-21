fcf30937b098dce37205df84dbfb50e6c3600df8
Artjom Kivonen <kivonen@gmail.com>
2024-09-21 13:05:00 +0300

    integrating rollbar

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -2,9 +2,19 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import App from './components/App';
 import resources from './locales/index.js';
 import store from './store/index';
+
+const rollbarConfig = {
+  accessToken: 'c01132175b194df485745d94d6433c54',
+  environment: 'testenv',
+};
+function TestError() {
+  const a = null;
+  return a.hello();
+}
 
 const init = async () => {
   const i18n = i18next.createInstance();
@@ -17,13 +27,18 @@ const init = async () => {
     });
 
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <I18nextProvider i18n={i18n}>
-          <App />
-        </I18nextProvider>
-      </BrowserRouter>
-    </Provider>
+    <RollbarProvider rollbarConfig={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <BrowserRouter>
+            <I18nextProvider i18n={i18n}>
+              <App />
+            </I18nextProvider>
+          </BrowserRouter>
+        </Provider>
+        <TestError />
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
